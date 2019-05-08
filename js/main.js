5d3cc9dab0bb7a8d74c6f6fc1671f95b9227ce49
gk <georgepulikkayath@gmail.com>
2019-05-07 23:34:31 -0400

    files updated

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 
     })
 })
-$("#bd").on("click",".btn",function () {
+$(".btn-lg").on("click",".btn",function () {
     debugger
     window.location ="https://github.com/georgepulikkayath";
 
